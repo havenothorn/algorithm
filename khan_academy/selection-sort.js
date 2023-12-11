9cb3adab49758fa5c4501b08a33bd04c5aa05898
Nami <havenothorn@gmail.com>
2023-12-11 20:53:13 +0900

    add: sub-array, selection-sort

---
+++ khan_academy/selection-sort.js
@@ -0,0 +1,31 @@
+var swap = function (array, firstIndex, secondIndex) {
+  var temp = array[firstIndex];
+  array[firstIndex] = array[secondIndex];
+  array[secondIndex] = temp;
+};
+
+var indexOfMinimum = function (array, startIndex) {
+  var minValue = array[startIndex];
+  var minIndex = startIndex;
+
+  for (var i = minIndex + 1; i < array.length; i++) {
+    if (array[i] < minValue) {
+      minIndex = i;
+      minValue = array[i];
+    }
+  }
+  return minIndex;
+};
+
+var selectionSort = function (array) {
+  var k;
+  for (var j = 0; j < array.length; j++) {
+    k = indexOfMinimum(array, j);
+    swap(array, j, k);
+  }
+};
+
+var array = [22, 11, 99, 88, 9, 7, 42];
+selectionSort(array);
+println("Array after sorting:  " + array);
+Program.assertEqual(array, [7, 9, 11, 22, 42, 88, 99]);
