b97adab35b9d14fe71bfd4b6d9399e23b95975c5
Nami <havenothorn@gmail.com>
2024-01-14 16:44:18 +0900

    add: Linked List push & pop

---
+++ udemy/LEETCODE/LinkedList/EXERCISE-LL-Push.js
@@ -0,0 +1,108 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  printList() {
+    let temp = this.head;
+    while (temp !== null) {
+      console.log(temp.value);
+      temp = temp.next;
+    }
+  }
+
+  getHead() {
+    if (this.head === null) {
+      console.log("Head: null");
+    } else {
+      console.log("Head: " + this.head.value);
+    }
+  }
+
+  getTail() {
+    if (this.tail === null) {
+      console.log("Tail: null");
+    } else {
+      console.log("Tail: " + this.tail.value);
+    }
+  }
+
+  getLength() {
+    console.log("Length: " + this.length);
+  }
+
+  makeEmpty() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    // node가 0개, 1개, 2개 이상인 경우 (총 3가지 시나리오)
+    if (!this.head) return undefined;
+    let temp = this.head;
+    let pre = this.head;
+    while (temp.next) {
+      pre = temp;
+      temp = temp.next;
+    }
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return temp;
+  }
+}
+
+function test() {
+  let myLinkedList = new LinkedList(1);
+  myLinkedList.makeEmpty();
+  myLinkedList.push(1);
+  myLinkedList.push(2);
+
+  myLinkedList.getHead();
+  myLinkedList.getTail();
+  myLinkedList.getLength();
+  console.log("\nLinked List:");
+  myLinkedList.printList();
+}
+
+test();
+
+/*
+    EXPECTED OUTPUT:
+    ----------------
+    Head: 1
+    Tail: 2
+    Length: 2
+
+    Linked List:
+    1
+    2
+
+*/
