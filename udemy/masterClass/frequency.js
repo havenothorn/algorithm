95f6f262f39d85932a6f3d077a7c486b19823c7f
Nami <havenothorn@gmail.com>
2024-02-09 10:04:26 +0900

    add: frequency counter

---
+++ udemy/masterClass/frequency.js
@@ -0,0 +1,22 @@
+// native solution
+// O(N^2)
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    let correctIndex = arr2.indexOf(arr1[i] ** 2);
+    if (correctIndex === -1) {
+      return false;
+    }
+    console.log(arr2);
+    arr2.splice(correctIndex, 1);
+  }
+  return true;
+}
+
+console.log(same([1, 2, 3, 2], [9, 1, 4, 4]));
+
+// console.log(same([1, 2, 3], [4, 1, 9])); // true
+// console.log(same([1, 2, 3], [1, 9])); // false
+// console.log(same([1, 2, 1], [4, 4, 1])); // false (must be same frequency)
