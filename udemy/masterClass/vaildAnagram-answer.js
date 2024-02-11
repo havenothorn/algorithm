00da4d3a14209cfe5ae188ea041c609490a07539
Nami <havenothorn@gmail.com>
2024-02-11 22:35:35 +0900

    add: anagram answer

---
+++ udemy/masterClass/vaildAnagram-answer.js
@@ -0,0 +1,26 @@
+function validAnagram(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+
+  const lookup = {};
+
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+  }
+  console.log(lookup);
+
+  for (let i = 0; i < second.length; i++) {
+    let letter = second[i];
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+  return true;
+}
+
+console.log(validAnagram("anagram", "margana"));
+// 문자열 a가 정확히 몇 번 나타나고 문자열마다 같은지를 확인해야한다.
