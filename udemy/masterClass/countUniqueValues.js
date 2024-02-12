63a6faf2e4f336b4e6027cf5585eab9426f8f644
Nami <havenothorn@gmail.com>
2024-02-12 13:54:35 +0900

    add: countUniqueValues

---
+++ udemy/masterClass/countUniqueValues.js
@@ -0,0 +1,18 @@
+// 정렬된 배열을 받아들이고 배열의 고유 값을 세는 countUniqueValues라는 함수를 구현합니다. 배열에 음수가 있을 수 있지만 항상 정렬됩니다.
+// self
+function countUniqueValues(arr) {
+  if (arr.length === 0) return 0;
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+console.log(countUniqueValues([1, 1, 1, 1, 1, 2]));
+// countUniqueValues([1, 1, 1, 1, 1, 2]); // 2
+// countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]); // 7
+// countUniqueValues([]); // 0
+// countUniqueValues([-2, -1, -1, 0, 1]); // 4
